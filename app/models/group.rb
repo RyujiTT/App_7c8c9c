8fc9a1a0b98fbf9823e043737da9ac03f56b0d9b
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-05-23 06:59:51 +0000

    [add]グループ作成機能

---
+++ app/models/group.rb
@@ -0,0 +1,13 @@
+class Group < ApplicationRecord
+  belongs_to :owner, class_name: 'User'
+  has_many :group_users, dependent: :destroy
+  has_many :users, through: :group_users
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  has_one_attached :group_image
+
+  def is_owned_by?(user)
+    owner.id == user.id
+  end
+end
