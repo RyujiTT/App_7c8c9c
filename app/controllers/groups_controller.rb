a62193de623593e09ad695bfaac8ab60ffce1b38
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-05-24 05:10:56 +0000

    [add]グループメンバーに対してのメール機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,6 +38,18 @@ class GroupsController < ApplicationController
     end
   end
 
+  def new_mail
+    @group = Group.find(params[:group_id])
+  end
+
+  def send_mail
+    @group = Group.find(params[:group_id])
+    group_users = @group.users
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+  end
+
+
   private
 
   def group_params
---
+++ app/mailers/event_mailer.rb
@@ -0,0 +1,6 @@
+class EventMailer < ApplicationMailer
+  def send_mail(mail_title, mail_content, group_users) #メソッドに対して引数を設定
+    @mail_title = mail_title
+    @mail_content = mail_content
+  end
+end
